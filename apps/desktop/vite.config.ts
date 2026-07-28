fd6c5afa0bd7622900462026cfe711f34d82c889
Welt <w3lt@negentropy.studio>
2026-07-28 13:45:05 +0200

    code(style): add prettier and eslint

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
+const host = process.env.TAURI_DEV_HOST
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
@@ -21,12 +21,12 @@ export default defineConfig(async () => ({
       ? {
           protocol: "ws",
           host,
-          port: 1421,
+          port: 1421
         }
       : undefined,
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
-  },
-}));
+      ignored: ["**/src-tauri/**"]
+    }
+  }
+}))
---
+++ eslint.config.ts
@@ -0,0 +1,19 @@
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+import { defineConfig, globalIgnores } from "eslint/config"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+
+export default defineConfig([
+  globalIgnores(["dist", "node_modules"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier
+])

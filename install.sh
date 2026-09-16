29825af9a540f0f22925dd01bb409d4267025ed6
Welt <w3lt@negentropy.studio>
2026-09-16 19:17:28 +0700

    ignore: wip

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Step 1: Download the desktop app to /Applications
+
+# Step 2: Configure env $LYS_HOME - lys home directory
+
+# Step 3: Download the backend to $LYS_HOME/runtime/backend/
+
+# Step 4: Download NodeJS to $LYS_HOME/runtime/node/
+
+# Step 5: Verify every components and print the result
